c49d8b9cec85b2a4b08b00b1ef314861f12c6d0c
Lars Erik Skraastad <lars.erik.skraastad@get.no>
2020-12-24 15:48:28 +0100

    add pwa and caching

--- web/packages/cook-book/next.config.js
+++ web/packages/cook-book/next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-    target: 'serverless',
-};
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+    }
+})
+//
+// module.exports = {
+//     target: 'serverless',
+// };
